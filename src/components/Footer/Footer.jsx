5af6f45aea29dc5f0295c32786f34e2e8ae6d862
samaira <147648652+samaira23@users.noreply.github.com>
2026-03-22 22:59:34 +0530

    Update Footer.jsx

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -17,42 +17,6 @@ export default function Footer() {
           </p>
         </div>
 
-        {/* Templates */}
-        <div>
-          <div className="footer__col-title">Templates</div>
-          <ul className="footer__links">
-            {['IEEE', 'ACM', 'Springer', 'Elsevier', 'APA', 'arXiv'].map((t) => (
-              <li key={t}>
-                <a href="#formatter" className="footer__link">{t}</a>
-              </li>
-            ))}
-          </ul>
-        </div>
-
-        {/* Resources */}
-        <div>
-          <div className="footer__col-title">Resources</div>
-          <ul className="footer__links">
-            <li><a href="#docs" className="footer__link">Documentation</a></li>
-            <li><a href="#" className="footer__link">GitHub</a></li>
-            <li><a href="#" className="footer__link">CLAUDE.md</a></li>
-            <li><a href="#" className="footer__link">Changelog</a></li>
-          </ul>
-        </div>
-
-        {/* Pipeline */}
-        <div>
-          <div className="footer__col-title">Pipeline</div>
-          <ul className="footer__links">
-            {['Extract', 'Parse', 'Canon', 'Normalize', 'Render', 'Compile'].map((s) => (
-              <li key={s}>
-                <a href="#how-it-works" className="footer__link">{s}</a>
-              </li>
-            ))}
-          </ul>
-        </div>
-      </div>
-
       {/* Bottom bar */}
       <div className="footer__bottom">
         <span className="footer__copy">
